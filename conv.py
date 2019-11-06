a6b5adb9668b1e3273ea633a1f3169ea1e1dfebe
evgenpatrushev <Patrushev99@gmail.com>
2019-11-06 22:57:24 +0200

    add padding and backprop for multi conv at conv layer

--- conv.py
+++ conv.py
@@ -4,15 +4,22 @@ import numpy as np
 class Convolution:
     # A Convolution layer using (shape x shape) filters.
 
-    def __init__(self, num_filters=1, shape=3, stride=1, image=False):
+    def __init__(self, num_filters=1, shape=3, stride=1, padding=0, image=False):
         self.image = image
         self.num_filters = num_filters
         self.stride = stride
+        self.padding = padding
         self.shape = shape
         # We divide by 9 to reduce the variance of our initial values
         self.filters = np.random.randn(num_filters, shape, shape) / 9
         self.biases = np.ones(num_filters)
         self.input, self.output = np.array([]), np.array([])
+
+    def padding_f(self, matrix, padding):
+        h, w = matrix.shape
+        res = np.zeros((h + 2 * padding, w + 2 * padding))
+        res[padding:-padding, padding:-padding] = matrix
+        return res
 
     def iterate_regions(self, image):
         '''
@@ -22,8 +29,8 @@ class Convolution:
         h, w = image.shape
 
         regions = ((i, j, image[i:(i + self.shape), j:(j + self.shape)])
-                   for i in range(0, h - self.shape + 1, self.stride)
-                   for j in range(0, w - self.shape + 1, self.stride))
+                   for i in range(0, h - self.shape + 1 + 2 * self.padding, self.stride)
+                   for j in range(0, w - self.shape + 1 + 2 * self.padding, self.stride))
 
         for i, j, region in regions:
             yield i, j, region
@@ -80,27 +87,33 @@ class Convolution:
         - input is a 2d numpy array
         '''
 
+        if self.padding:
+            input_val = self.padding_f(input_val, self.padding)
+
         h, w = input_val.shape
         self.output = np.zeros(
-            ((h - self.shape) // self.stride + 1, (w - self.shape) // self.stride + 1, self.num_filters))
+            ((h - self.shape + 2 * self.padding) // self.stride + 1,
+             (w - self.shape + 2 * self.padding) // self.stride + 1, self.num_filters))
 
         for i, j, im_region in self.iterate_regions(input_val):
             self.output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
 
         return self.output
 
-    def back_propagation(self, d_L_d_out, learn_rate):
+    def back_propagation(self, d_l_d_out, learn_rate):
+
+        # TODO assert if d_l_d_out shape != input shape
 
         if self.image:
 
             if len(self.input.shape) == 2:
-                self._backprop(d_L_d_out, learn_rate, self.input)
+                self._backprop(d_l_d_out, learn_rate, self.input)
 
             elif len(self.input.shape) == 3:
                 filters = self.filters
                 for i, img_color, filters_for_color in enumerate(zip(self.input, filters)):
                     self.filters = filters_for_color
-                    self._backprop(d_L_d_out=d_L_d_out, learn_rate=learn_rate, input_val=img_color)
+                    self._backprop(d_l_d_out=d_l_d_out, learn_rate=learn_rate, input_val=img_color)
                     filters[i] = self.filters
 
         else:
@@ -108,30 +121,60 @@ class Convolution:
             d_L_d_out have to be matrix same size as output of forward function 
             but with error for backprop
             """
+            # TODO -sure about backprop at multiple conv obj with same filters (formula) ... look good but not sure
             num_conv = self.input.shape[2]
+            return_val = np.zeros(self.input)
 
             for i in range(num_conv):
-                self._backprop(d_L_d_out=d_L_d_out[:, :, i * self.shape:i + self.shape], learn_rate=learn_rate,
-                               input_val=self.input[:, :, i])
-                # TODO -sure about backprop at multiple conv obj with same filters (formula) ... look good but not sure
+                # save variant of backprop, but i thnk coeff not correct
+                # self._backprop(d_l_d_out=d_l_d_out[:, :, i * self.shape:i + self.shape], learn_rate=learn_rate,
+                #                input_val=self.input[:, :, i])
 
-    def _backprop(self, d_L_d_out, learn_rate, input_val):
+                return_val[:, :, i] = self._backprop(
+                    d_l_d_out=d_l_d_out[:, :, i * self.num_filters:i * self.num_filters + self.num_filters],
+                    learn_rate=learn_rate, input_val=self.input[:, :, i])
+            return return_val
+
+    def _backprop(self, d_l_d_out, learn_rate, input_val):
         '''
         Performs a backward pass of the conv layer.
         - d_L_d_out is the loss gradient for this layer's outputs.
         - learn_rate is a float.
 
         '''
-        d_L_d_filters = np.zeros(self.filters.shape)
+        d_l_d_filters = np.zeros(self.filters.shape)
+
+        if self.padding:
+            input_val = self.padding_f(input_val, self.padding)
 
         for i, j, im_region in self.iterate_regions(input_val):
             for f in range(self.num_filters):
-                d_L_d_filters[f] += d_L_d_out[i, j, f] * im_region
+                d_l_d_filters[f] += d_l_d_out[i, j, f] * im_region
 
         # Update filters
-        self.filters -= learn_rate * d_L_d_filters
+        self.filters -= learn_rate * d_l_d_filters
 
         # TODO -add biases update
+
+        if not self.image:
+            d_l_d_x = np.zeros(input_val.shape)
+            d_l_d_x = d_l_d_x[..., np.newaxis]
+
+            padding = self.shape - 1 - self.padding
+
+            # TODO -check this part of logic
+            for f in range(self.num_filters):
+
+                if padding:
+                    iter_val = self.padding_f(d_l_d_out[:, :, f], padding)
+                else:
+                    iter_val = d_l_d_out[:, :, f]
+
+                w = np.rot90(self.filters[f], 2)
+
+                for i, j, im_region in self.iterate_regions(iter_val):
+                    d_l_d_x[i, j, 0] += np.sum(im_region * w)
+            return d_l_d_x
 
         return None
 
@@ -139,5 +182,4 @@ class Convolution:
 if __name__ == '__main__':
     obj = Convolution(num_filters=2)
     print(list(obj.iterate_regions(image=np.array([[0, 50, 0, 29], [0, 80, 31, 2], [33, 90, 0, 75], [0, 9, 0, 95]]))))
-    o = obj.forward(input_val=np.array([[0, 50, 0, 29], [0, 80, 31, 2], [33, 90, 0, 75], [0, 9, 0, 95]]))
-    print(o)
+    obj.forward(input_val=np.array([[0, 50, 0, 29], [0, 80, 31, 2], [33, 90, 0, 75], [0, 9, 0, 95]]))
