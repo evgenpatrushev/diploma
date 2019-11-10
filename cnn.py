add cnn train, update conv, update softmax back-prop bug: too big values of d_l_d_out - problem with back-prop problem: doesnt work ReLu

cnn.py
@@ -3,6 +3,7 @@ import numpy as np
 from conv import Convolution
 from maxpool import MaxPool
 from softmax import Softmax
+from ReLu import ReLU
 
 # We only use the first 1k examples of each set in the interest of time.
 # Feel free to change this if you want.
@@ -10,104 +11,52 @@ train_images = mnist.train_images()[:1000]
 train_labels = mnist.train_labels()[:1000]
 test_images = mnist.test_images()[:1000]
 test_labels = mnist.test_labels()[:1000]
-
-l1_conv_start = Convolution(3, image=True)  # 28x28x1 -> 26x26x3
-l2_pool = MaxPool()  # 26x26x8 -> 13x13x8
-l3_conv = Convolution(5)  # 28x28x1 -> 26x26x3
-l4_conv = Convolution(5, shape=2)  # 28x28x1 -> 26x26x3
-l5_pool = MaxPool()  # 26x26x8 -> 13x13x8
-softmax = Softmax(5 * 5 * 75, 10)  # 13x13x8 -> 10
 lr = 0.005
+load_weights = False
 
-for img, label in zip(train_images, train_labels):
-    l1_conv_start.forward(img)
-    l2_pool.forward(l1_conv_start.output)
-    l3_conv.forward(l2_pool.output)
-    l4_conv.forward(l3_conv.output)
-    l5_pool.forward(l4_conv.output)
-    softmax.forward(l5_pool.output)
-
-    d_l_d_out = -1 * label / softmax.out + (1 - label) / (1 - softmax.out)
-    d_l_d_out = softmax.backprop(d_l_d_out, lr)
-    d_l_d_out = l5_pool.backprop(d_l_d_out)
-    d_l_d_out = l4_conv.back_propagation(d_l_d_out, lr)
-    d_l_d_out = l2_pool.backprop(d_l_d_out)
-    d_l_d_out = l3_conv.back_propagation(d_l_d_out, lr)
-    l1_conv_start.back_propagation(d_l_d_out, lr)
+l1_conv_start = Convolution(num_filters=8, shape=3, image=True)  # 28x28x1 -> 26x26x8
+l2_pool = MaxPool(size_of_pool=2)  # 26x26x8 -> 13x13x8
+l3_conv = Convolution(num_filters=8, shape=4)  # 13x13x8-> 10x10x64
+l4_pool = MaxPool(size_of_pool=2)  # 10x10x64 -> 5x5x64
+l5_conv = Convolution(num_filters=5, shape=3)  # 5x5x64 -> 3x3x320
+l6_pool = MaxPool(size_of_pool=3)  # 3x3x320 -> 1x1x320
+softmax = Softmax(1 * 1 * 320, 10)  # 13x13x8 -> 10
 
 
-class CNN():
-    def __init__(self):
-        pass
+def train(train_x, train_y, p=True):
 
-    def fit(self, image, label):
-        out = conv.forward((image / 255) - 0.5)
-        out = pool.forward(out)
-        out = softmax.forward(out)
-
-        # Calculate cross-entropy loss and accuracy. np.log() is the natural log.
-        loss = -np.log(out[label])
-        acc = 1 if np.argmax(out) == label else 0
-
-
-def forward(image, label):
-    '''
-    Completes a forward pass of the CNN and calculates the accuracy and
-    cross-entropy loss.
-    - image is a 2d numpy array
-    - label is a digit
-    '''
-    # We transform the image from [0, 255] to [-0.5, 0.5] to make it easier
-    # to work with. This is standard practice.
-    out = conv.forward((image / 255) - 0.5)
-    out = pool.forward(out)
-    out = softmax.forward(out)
-
-    # Calculate cross-entropy loss and accuracy. np.log() is the natural log.
-    loss = -np.log(out[label])
-    acc = 1 if np.argmax(out) == label else 0
-
-    return out, loss, acc
-
-
-def train(im, label, lr=.005):
-    '''
-    Completes a full training step on the given image and label.
-    Returns the cross-entropy loss and accuracy.
-    - image is a 2d numpy array
-    - label is a digit
-    - lr is the learning rate
-    '''
-    # Forward
-    out, loss, acc = forward(im, label)
-
-    # Calculate initial gradient
-    gradient = -1 / (label * out) + (1 - label) / (1 - out)
-
-    # Backprop
-    gradient = softmax.backprop(gradient, lr)
-    gradient = pool.backprop(gradient)
-    gradient = conv.backprop(gradient, lr)
-
-    return loss, acc
-
-
-def run_old_cnn():
-    print('MNIST CNN initialized!')
-
-    # Train the CNN for 3 epochs
-    for epoch in range(3):
+    if p:
+        print('train with permutation')
+    else:
+        print('train without permutation')
+    for epoch in range(4):
         print('--- Epoch %d ---' % (epoch + 1))
 
         # Shuffle the training data
-        permutation = np.random.permutation(len(train_images))
-        train_images = train_images[permutation]
-        train_labels = train_labels[permutation]
+        if p:
+            permutation = np.random.permutation(len(train_x))
+            train_img = train_x[permutation]
+            train_l = train_y[permutation]
+        else:
+            train_img = train_x
+            train_l = train_y
 
-        # Train!
-        loss = 0
-        num_correct = 0
-        for i, (im, label) in enumerate(zip(train_images, train_labels)):
+        loss, num_correct = 0, 0
+
+        for i, obj in enumerate(zip(train_img, train_l)):
+            img, l = obj
+
+            l1_conv_start.forward(img)
+            l2_pool.forward(l1_conv_start.output)
+            l3_conv.forward(l2_pool.output)
+            l4_pool.forward(l3_conv.output)
+            l5_conv.forward(l4_pool.output)
+            l6_pool.forward(l5_conv.output)
+            softmax.forward(l6_pool.output)
+
+            label = np.zeros(10)
+            label[l] = 1
+
             if i % 100 == 99:
                 print(
                     '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
@@ -116,19 +65,49 @@ def run_old_cnn():
                 loss = 0
                 num_correct = 0
 
-            l, acc = train(im, label)
-            loss += l
-            num_correct += acc
+            loss += -np.log(softmax.out[l])
+            num_correct += 1 if np.argmax(softmax.out) == l else 0
+
+            d_l_d_out = -1 * label / softmax.out
+
+            d_l_d_out = softmax.backprop(d_l_d_out, lr)
+            d_l_d_out = l6_pool.backprop(d_l_d_out)
+            d_l_d_out = l5_conv.back_propagation(d_l_d_out, lr)
+            d_l_d_out = l4_pool.backprop(d_l_d_out)
+            d_l_d_out = l3_conv.back_propagation(d_l_d_out, lr)
+            d_l_d_out = l2_pool.backprop(d_l_d_out)
+            l1_conv_start.back_propagation(d_l_d_out, lr)
 
     # Test the CNN
     print('\n--- Testing the CNN ---')
     loss = 0
     num_correct = 0
-    for im, label in zip(test_images, test_labels):
-        _, l, acc = forward(im, label)
-        loss += l
-        num_correct += acc
+    for img, label in zip(test_images, test_labels):
+        img = (img / 255) - 0.5
+
+        l1_conv_start.forward(img)
+        l2_pool.forward(l1_conv_start.output)
+        l3_conv.forward(l2_pool.output)
+        l4_pool.forward(l3_conv.output)
+        l5_conv.forward(l4_pool.output)
+        l6_pool.forward(l5_conv.output)
+        softmax.forward(l6_pool.output)
+
+        loss += -np.log(softmax.out[label])
+        num_correct += 1 if np.argmax(softmax.out) == label else 0
 
     num_tests = len(test_images)
     print('Test Loss:', loss / num_tests)
     print('Test Accuracy:', num_correct / num_tests)
+
+
+class CNN():
+    def __init__(self):
+        pass
+
+    def fit(self, image, label):
+        pass
+
+
+if __name__ == '__main__':
+    train(train_images, train_labels)

conv.py
@@ -10,9 +10,12 @@ class Convolution:
         self.stride = stride
         self.padding = padding
         self.shape = shape
-        # We divide by 9 to reduce the variance of our initial values
-        self.filters = np.random.randn(num_filters, shape, shape) / 9
-        self.biases = np.ones(num_filters)
+
+        # self.filters = np.random.normal(loc=0, scale=0.2, size=(num_filters, shape, shape))
+        self.filters = np.random.normal(loc=0, scale=5, size=(num_filters, shape, shape)) / (
+                self.num_filters + self.shape + self.shape)
+
+        self.biases = np.zeros(num_filters)
         self.input, self.output = np.array([]), np.array([])
 
     def padding_f(self, matrix, padding):
@@ -29,8 +32,8 @@ class Convolution:
         h, w = image.shape
 
         regions = ((i, j, image[i:(i + self.shape), j:(j + self.shape)])
-                   for i in range(0, h - self.shape + 1 + 2 * self.padding, self.stride)
-                   for j in range(0, w - self.shape + 1 + 2 * self.padding, self.stride))
+                   for i in range(0, h - self.shape + 1, self.stride)
+                   for j in range(0, w - self.shape + 1, self.stride))
 
         for i, j, region in regions:
             yield i, j, region
@@ -40,31 +43,34 @@ class Convolution:
 
             if len(input_val.shape) == 2:
                 self._forward(input_val=input_val)
+                self.output = self.output + self.biases
 
             elif len(input_val.shape) == 3:
-                """
-                RGB images are splitting into three color layers and for each color are created filter and then 
-                sum into one output (image/conv) with bias.
-                
-                Combination of 3 filters are created num_filters times
-                
-                Algorithm: 
-                    1 - get 3 color RGB layers
-                    2 - random filters for each RGB layer created 'num_filters' filters with shape 'shape*shape'
-                    3 - by num_filters convoluted each RGB layer and then sum for each RGB into one 
-                    4 - get np array with shape (shape, shape, num_filters)   
-                """
-                num_filters, shape = self.filters.shape
-                filters = np.random.randn(3, num_filters, shape, shape) / 9
-                output = 0
+                    """
+                    RGB images are splitting into three color layers and for each color are created filter and then 
+                    sum into one output (image/conv) with bias.
+                    
+                    Combination of 3 filters are created num_filters times
+                    
+                    Algorithm: 
+                        1 - get 3 color RGB layers
+                        2 - random filters for each RGB layer created 'num_filters' filters with shape 'shape*shape'
+                        3 - by num_filters convoluted each RGB layer and then sum for each RGB into one 
+                        4 - get np array with shape (shape, shape, num_filters)   
+                    """
 
-                for img_color, filter_for_color in zip(input_val, filters):
-                    self.filters = filter_for_color
-                    self._forward(input_val=img_color)
-                    output += self.output
+                    filters = np.random.normal(loc=0, scale=5, size=(3, self.num_filters, self.shape, self.shape)) / (
+                            self.num_filters + self.shape + self.shape)
+                    # filters = np.random.normal(loc=0, scale=0.2, size=(3, self.num_filters, self.shape, self.shape))
+                    output = 0
 
-                self.filters = filters
-                self.output = output + self.biases
+                    for img_color, filter_for_color in zip(input_val, filters):
+                        self.filters = filter_for_color
+                        self._forward(input_val=img_color)
+                        output += self.output
+
+                    self.filters = filters
+                    self.output = output + self.biases
 
         else:
             num_conv = input_val.shape[2]
@@ -92,8 +98,8 @@ class Convolution:
 
         h, w = input_val.shape
         self.output = np.zeros(
-            ((h - self.shape + 2 * self.padding) // self.stride + 1,
-             (w - self.shape + 2 * self.padding) // self.stride + 1, self.num_filters))
+            ((h - self.shape) // self.stride + 1,
+             (w - self.shape) // self.stride + 1, self.num_filters))
 
         for i, j, im_region in self.iterate_regions(input_val):
             self.output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
@@ -123,7 +129,7 @@ class Convolution:
             """
             # TODO -sure about backprop at multiple conv obj with same filters (formula) ... look good but not sure
             num_conv = self.input.shape[2]
-            return_val = np.zeros(self.input)
+            return_val = np.zeros(self.input.shape)
 
             for i in range(num_conv):
                 # save variant of backprop, but i thnk coeff not correct
@@ -133,6 +139,7 @@ class Convolution:
                 return_val[:, :, i] = self._backprop(
                     d_l_d_out=d_l_d_out[:, :, i * self.num_filters:i * self.num_filters + self.num_filters],
                     learn_rate=learn_rate, input_val=self.input[:, :, i])
+                # TODO -add biases update
             return return_val
 
     def _backprop(self, d_l_d_out, learn_rate, input_val):
@@ -140,25 +147,12 @@ class Convolution:
         Performs a backward pass of the conv layer.
         - d_L_d_out is the loss gradient for this layer's outputs.
         - learn_rate is a float.
-
         '''
-        d_l_d_filters = np.zeros(self.filters.shape)
 
-        if self.padding:
-            input_val = self.padding_f(input_val, self.padding)
-
-        for i, j, im_region in self.iterate_regions(input_val):
-            for f in range(self.num_filters):
-                d_l_d_filters[f] += d_l_d_out[i, j, f] * im_region
-
-        # Update filters
-        self.filters -= learn_rate * d_l_d_filters
-
-        # TODO -add biases update
-
+        # Create return val if it's not first layer
+        return_val = None
         if not self.image:
             d_l_d_x = np.zeros(input_val.shape)
-            d_l_d_x = d_l_d_x[..., np.newaxis]
 
             padding = self.shape - 1 - self.padding
 
@@ -173,10 +167,25 @@ class Convolution:
                 w = np.rot90(self.filters[f], 2)
 
                 for i, j, im_region in self.iterate_regions(iter_val):
-                    d_l_d_x[i, j, 0] += np.sum(im_region * w)
-            return d_l_d_x
+                    d_l_d_x[i, j] += np.sum(im_region * w)
+            return_val = d_l_d_x
 
-        return None
+        # Update filters and biases
+        d_l_d_filters = np.zeros(self.filters.shape)
+
+        if self.padding:
+            input_val = self.padding_f(input_val, self.padding)
+
+        for i, j, im_region in self.iterate_regions(input_val):
+            for f in range(self.num_filters):
+                d_l_d_filters[f] += d_l_d_out[i, j, f] * im_region
+
+        # Update filters
+        self.filters -= learn_rate * d_l_d_filters
+        self.biases -= learn_rate * np.sum(d_l_d_out, axis=(0, 1))
+        # TODO -add biases update
+
+        return return_val
 
 
 if __name__ == '__main__':

maxpool.py
@@ -4,7 +4,7 @@ import warnings
 
 class MaxPool:
     # A Max Pooling layer using a pool size of 2.
-    def __init__(self, size_of_pool=2):
+    def __init__(self, size_of_pool=4):
         self.size_of_pool = size_of_pool
         self.input, self.output = np.array([]), np.array([])
 

softmax.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+np.seterr(all='raise')
+
 
 class Softmax:
     # A standard fully-connected layer with softmax activation.
 
     def __init__(self, input_len, nodes):
         # We divide by input_len to reduce the variance of our initial values
-        self.weights = np.random.randn(input_len, nodes) / input_len
-        self.biases = np.ones(nodes)
+        self.weights = np.random.normal(loc=0, scale=5, size=(input_len, nodes)) / input_len
+        # self.weights = np.random.normal(loc=0, scale=0.2, size=(input_len, nodes))
+        self.biases = np.zeros(nodes)
         self.input_shape, self.input, self.totals, self.out = (), np.array([]), np.array([]), np.array([])
 
     def forward(self, input_val):
@@ -18,7 +21,8 @@ class Softmax:
         '''
         self.input_shape = input_val.shape
 
-        input_val = input_val.flatten()
+        input_val = input_val.flatten() / np.max(input_val)
+        # input_val = input_val.flatten()
         self.input = input_val
 
         totals = np.dot(input_val, self.weights) + self.biases
@@ -36,48 +40,41 @@ class Softmax:
         - learn_rate is a float.
         '''
 
+        for i, gradient in enumerate(d_l_d_out):
+            if gradient == 0:
+                continue
+
+            exp = np.exp(self.totals)
+            s = np.sum(exp)
+
+            # Gradients of out[i] against totals
+            d_out_d_in = - exp[i] * exp / (s ** 2)
+            d_out_d_in[i] = exp[i] * (s - exp[i]) / (s ** 2)
+
+            d_l_d_x = np.dot(self.weights, (gradient * d_out_d_in)[..., np.newaxis]).reshape(self.input_shape)
+
+            self.weights = self.weights - learn_rate * np.dot(self.input[..., np.newaxis],
+                                                              (gradient * d_out_d_in)[np.newaxis, ...])
+
+            self.biases = self.biases - learn_rate * gradient * d_out_d_in
+
+            return d_l_d_x
+
         exp = np.exp(self.totals)
         s = np.sum(exp)
 
         d_out_d_in = [(exp[i] * np.sum(list(exp[:i]) + list(exp[i + 1:]))) / (s ** 2) for i in range(len(exp))]
-        self.weights = self.weights - learn_rate * np.dot(self.input[np.newaxis, ...].T,
-                                                          (d_l_d_out * d_out_d_in)[np.newaxis, ...])
+        self.weights = self.weights - learn_rate * np.dot(self.input[..., np.newaxis],
+                                                          (gradient * d_out_d_in)[np.newaxis, ...])
         self.biases = self.biases - learn_rate * d_l_d_out * d_out_d_in
+        return np.dot(self.weights, (d_l_d_out * d_out_d_in)[..., np.newaxis]).reshape(self.input_shape)
 
-        # previous version --------------------------------------------------------------------------------------------
-        # We know only 1 element of d_L_d_out will be nonzero
-        # for i, gradient in enumerate(d_L_d_out):
-        #     if gradient == 0:
-        #         continue
-        #
-        #     # e^totals
-        #     t_exp = np.exp(self.last_totals)
-        #
-        #     # Sum of all e^totals
-        #     S = np.sum(t_exp)
-        #
-        #     # Gradients of out[i] against totals
-        #     d_out_d_t = -t_exp[i] * t_exp / (S ** 2)
-        #     d_out_d_t[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)
-        #
-        #     # Gradients of totals against weights/biases/input
-        #     d_t_d_w = self.last_input
-        #     d_t_d_b = 1
-        #     d_t_d_inputs = self.weights
-        #
-        #     # Gradients of loss against totals
-        #     d_L_d_t = gradient * d_out_d_t
-        #
-        #     # Gradients of loss against weights/biases/input
-        #     d_L_d_w = d_t_d_w[np.newaxis].T @ d_L_d_t[np.newaxis]
-        #     d_L_d_b = d_L_d_t * d_t_d_b
-        #     d_L_d_inputs = d_t_d_inputs @ d_L_d_t
-        #
-        #     # Update weights / biases
-        #     self.weights -= learn_rate * d_L_d_w
-        #     self.biases -= learn_rate * d_L_d_b
 
-        # return d_L_d_inputs.reshape(self.last_input_shape)
-        # -------------------------------------------------------------------------------------------------------------
-
-        return (d_l_d_out * d_out_d_in).reshape(self.input_shape)
+if __name__ == '__main__':
+    obj = Softmax(3, 3)
+    obj.weights = np.array([[0.1, 0.4, 0.8], [0.3, 0.7, 0.2], [0.5, 0.2, 0.9]])
+    obj.forward(input_val=np.array([0.938, 0.94, 0.98]))
+    label = np.array([1, 0, 0])
+    obj.out = np.array([0.26980, 0.32235, 0.40784])
+    d_l_d_out = -1 * label / obj.out - (1 - label) / (1 - obj.out)
+    obj.backprop(d_l_d_out, 0.01)